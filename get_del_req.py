1d7375f99e304c1c5e4c656da6d6a10fd93a90c3
Ivan Fedun <ivan.fedun@gmail.com>
2014-11-18 21:14:36 +0200

    modified test script

--- get_del_req.py
+++ get_del_req.py
@@ -32,19 +32,31 @@ def get_candidate_by_id(user_id):
 #get_candidate_by_id('18');
 #get_candidate_by_id('3');
 
-def post_candidate(candidateName, candidatePosition):
+def post_candidate(candidateName, candidatePosition, contentType):
 	url = 'http://qainterview.cogniance.com/candidates'
-	payload = {'name': candidateName, 'position': candidatePosition}
+	#payload = {'name': candidateName, 'position': candidatePosition}
 	#payload = dict(name=candidateName, position=candidatePosition)
-	headers = {'content-type': 'application/json'}
+	payload = dict()
+	if candidateName is not None:
+		payload['name']=candidateName
+	if candidatePosition is not None:
+		payload['position']=candidatePosition
+	headers = dict() 
+	if contentType is not None:
+		headers['content-type']=contentType
+	#headers = {'content-type': contentType}
 	r = requests.post(url, data=json.dumps(payload), headers=headers)
+	print(json.dumps(payload))
+	print(headers)
+	print(r.text)
+	print(r.status_code)
 	return r;
 
 #post_candidate('Baba jaga', 'rider' );
 
 
 def test_post_candidate():
-	r = post_candidate('bad', 'rr');
+	r = post_candidate('robert' , 'reader', 'application/json');
 	if (r.status_code == 201) and (r.text !=''):
 		print 'test passed'
 	else:
