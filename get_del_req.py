7e46d158e41c4c0a6b553d1ea0e8f802cb99ee31
Ivan Fedun <ivan.fedun@gmail.com>
2014-11-19 04:57:39 +0200

    new fishka add

--- get_del_req.py
+++ get_del_req.py
@@ -5,32 +5,33 @@ import json
 def get_candidates():
 	url = 'http://qainterview.cogniance.com/candidates'
 	r = requests.get(url)
+	data =json.loads(r.text)
+	candidateList = data['candidates']
+	print candidateList[0]['id']
+	print len(candidateList)
 	#print(r.text)
 	#print(r.status_code)
 	return r;
 
-#get_candidates();
+get_candidates();
 
-def test_get1():
+def test_get_candidates():
 	r = get_candidates();
 	if (r.status_code == 200) and (r.text !=''):
 		print 'test passed'
 	else:
-		print 'failed'
+		print 'test failed'
 	return;
 
-#test_get1();
+#test_get_candidates();
 
 def get_candidate_by_id(user_id):
-	payload = user_id
 	url = 'http://qainterview.cogniance.com/candidates/'
-	r = requests.get(url + payload)
-	print(r.text)
-	print(r.status_code)
-	return;
+	r = requests.get(url + str(user_id))
+	return r;
 
-#get_candidate_by_id('18');
-#get_candidate_by_id('3');
+#get_candidate_by_id(18);
+#get_candidate_by_id(3);
 
 def post_candidate(candidateName, candidatePosition, contentType):
 	url = 'http://qainterview.cogniance.com/candidates'
@@ -46,14 +47,20 @@ def post_candidate(candidateName, candidatePosition, contentType):
 		headers['content-type']=contentType
 	#headers = {'content-type': contentType}
 	r = requests.post(url, data=json.dumps(payload), headers=headers)
-	print(json.dumps(payload))
-	print(headers)
+	#print(json.dumps(payload))
+	#print(headers)
 	print(r.text)
-	print(r.status_code)
+	#print(r.status_code)
 	return r;
 
-#post_candidate('Baba jaga', 'rider' );
+#post_candidate('Baba jaga', 'rider', 'application/json' );
 
+def delete_candidate_by_id(user_id):
+	url = 'http://qainterview.cogniance.com/candidates/'
+	r = requests.delete (url + str(user_id))
+	return r;
+
+#delete_candidate_by_id(25);
 
 def test_post_candidate():
 	r = post_candidate('robert' , 'reader', 'application/json');
@@ -63,16 +70,45 @@ def test_post_candidate():
 		print 'test failed'
 	return;
 
-test_post_candidate();
+#test_post_candidate();
 
-def delete_candidate(cand_id):
-	payload = cand_id
-	url = 'http://qainterview.cogniance.com/candidates/'
-	r = requests.delete(url + payload)
-	print(r.text)
-	print(r.status_code)
+def test_get_by_id():
+	# preparing
+	response = post_candidate('rabbit' , 'zlo', 'application/json');
+	json_data = json.loads(response.text)
+	id = json_data['candidate']['id']
+	
+	# testing
+	r = get_candidate_by_id(id);
+	json_data2 = json.loads(r.text)
+	id2 = json_data2 ['candidate']['id']
+	
+	# checking
+	if (id == id2) and (r.status_code == 200):
+		print 'test passed'
+	else:
+		print 'test failed'
+	# cleaning
+	d = delete_candidate_by_id(id);
+	print(id,id2)
+	print 'status code', (r.status_code)
 	return;
 
-#delete_candidate('18');
-#delete_candidate('3');
+#test_get_by_id();
 
+def test_delete_by_id():
+	#preparing	
+	p = post_candidate('Romeo', 'worker', 'application/json');
+	json_data = json.loads(p.text)
+	id = json_data ['candidate']['id']
+	#testing	
+	d = delete_candidate_by_id(id);
+	#checking
+	g = get_candidate_by_id(id);
+	if (g.status_code != 200):
+		print 'test passed'
+	else:
+		print 'test failed'
+	return;
+
+#test_delete_by_id();
